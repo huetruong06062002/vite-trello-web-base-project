623d4f44b22e31ba9873110a54c846d200375f9c
Đệ giáo-làng <truongvshieu01@gmail.com>
2024-02-10 17:48:59 +0700

    Code layout Trello

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,27 @@
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import HomeIcon from "@mui/icons-material/Home";
 
-import { pink } from "@mui/material/colors";
-import Typography from "@mui/material/Typography";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import {
-  Experimental_CssVarsProvider as CssVarsProvider,
-  experimental_extendTheme as extendTheme,
-  useColorScheme,
-} from "@mui/material/styles";
+import { useColorScheme } from "@mui/material/styles"
 
-import * as React from "react";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import Box from "@mui/material/Box";
 
-import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
-import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import Box from '@mui/material/Box'
+
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import Container from '@mui/material/Container'
 
 function ModeSelect() {
-  const { mode, setMode } = useColorScheme();
+  const { mode, setMode } = useColorScheme()
 
   const handleChange = (event) => {
-    const selectedMode = event.target.value;
-    console.log("selectedMode:", selectedMode);
+    const selectedMode = event.target.value
+    // console.log("selectedMode:", selectedMode)
     // setAge(event.target.value)
-    setMode(selectedMode);
-  };
+    setMode(selectedMode)
+  }
 
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
@@ -64,58 +53,39 @@ function ModeSelect() {
         </MenuItem>
       </Select>
     </FormControl>
-  );
-}
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme("123");
-
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const prefersDarkLight = useMediaQuery("(prefers-color-scheme: light)");
-
-  console.log("prefersDarkMode: ", prefersDarkMode);
-  console.log("prefersDarkLight: ", prefersDarkLight);
-  console.log("");
-
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === "light" ? "dark" : "light");
-      }}
-    >
-      {mode === "light" ? "Turn dark" : "Turn light"}
-    </Button>
-  );
+  )
 }
 
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div>trungquandev</div>
-
-      <Typography variant="body2" color="text.secondary">
-        Test Typography
-      </Typography>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-        <AccessAlarmIcon />
-        <ThreeDRotation />
-        <HomeIcon />
-        <HomeIcon color="primary" />
-        <HomeIcon color="secondary" />
-        <HomeIcon color="success" />
-        <HomeIcon color="action" />
-        <HomeIcon color="disabled" />
-        <HomeIcon sx={{ color: pink[900] }} />
-      </Stack>
-    </>
-  );
+    <Container disableGutters maxWidth='false' sx={{ height: '100vh' }}>     
+      <Box sx={{
+        backgroundColor: 'primary.light',
+        width: '100%',
+        height: (theme) => theme.trello.appBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <ModeSelect />
+      </Box >
+      <Box sx={{
+        backgroundColor: 'primary.dark',
+        width: '100%',
+        height: (theme) => theme.trello.boardBarHeight,
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        Board Bar
+      </Box>
+      <Box sx={{
+        backgroundColor: 'primary.main',
+        width: '100%',      
+        height: (theme) => `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
+        alignItems: 'center'
+      }}>board content</Box>
+      <Box></Box>
+    </Container>
+  )
 }
 
-export default App;
+export default App
